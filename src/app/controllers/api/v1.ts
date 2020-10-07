936ca1e2b2a4d2f0386b93293d5800014b1cc68f
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-10-07 11:40:07 -0400

    feat: add controller for v1 sub router

---
+++ src/app/controllers/api/v1.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { LengthOfString } from './types';
+/* 
+  the returned data from server holds a firstName and lastName 
+  property both properties hold a string value that has a max length.
+  the whole string is filled with the name and if the name does not
+  fill out the max length.. empty slots are substituted for 0 to fill
+  out the nessacarry length of the string.
+*/
+
+class Apiv1Controller {
+  static parseBody(req: Request, res: Response): Response {
+    /* based on the set length of string we can calculate how to
+     slice the needed information */
+    const { data } = req.body;
+    const firstName = data.slice(0, LengthOfString.FirstName);
+    const lastName = data.slice(
+      LengthOfString.FirstName,
+      LengthOfString.FirstName + LengthOfString.LastName
+    );
+    const clientId = data.slice(
+      LengthOfString.FirstName + LengthOfString.LastName
+    );
+    return res.status(200).send({ firstName, lastName, clientId });
+  }
+}
+
+export default Apiv1Controller;
